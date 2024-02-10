38d1f720e15edfa65394807cad3a68efee50bf71
Maxim <kmaxusk@gmail.com>
2024-02-10 13:12:42 +0200

    task-4-completed

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,29 @@
 // const tableHeaders = ['№', 'price', 'amount', 'date'];
+import style from './CryptoHistory.module.css';
+import { formatToStats } from '../../utils/dateFormat';
 
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+export const CryptoHistory = ({ transactions }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          <th className={style.th}>№</th>
+          <th className={style.th}>PRICE</th>
+          <th className={style.th}>AMOUNT</th>
+          <th className={style.th}>DATE</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, price, amount, date }, idx) => (
+          <tr key={id} className={style.tr}>
+            <td className={style.td}>{idx+1}</td>
+            <td className={style.td}>{price}</td>
+            <td className={style.td}>{amount}</td>
+            <td className={style.td}>{formatToStats(date)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
